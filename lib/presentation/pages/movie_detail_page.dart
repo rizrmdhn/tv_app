c5ea6408e6543a368259beabad95caaf4c3e7073
rizrmdhn <rizrmdhn.work@gmail.com>
2023-11-11 14:07:31 +0800

    add message when reccomendations is empty

--- lib/presentation/pages/movie_detail_page.dart
+++ lib/presentation/pages/movie_detail_page.dart
@@ -199,45 +199,57 @@ class DetailContent extends StatelessWidget {
                                 } else if (data.recommendationState ==
                                     RequestState.loaded) {
                                   return SizedBox(
-                                    height: 150,
-                                    child: ListView.builder(
-                                      scrollDirection: Axis.horizontal,
-                                      itemBuilder: (context, index) {
-                                        final movie = recommendations[index];
-                                        return Padding(
-                                          padding: const EdgeInsets.all(4.0),
-                                          child: InkWell(
-                                            onTap: () {
-                                              Navigator.pushReplacementNamed(
-                                                context,
-                                                MovieDetailPage.routeName,
-                                                arguments: movie.id,
-                                              );
-                                            },
-                                            child: ClipRRect(
-                                              borderRadius:
-                                                  const BorderRadius.all(
-                                                Radius.circular(8),
+                                      height: 150,
+                                      child: recommendations.isEmpty
+                                          ? const Center(
+                                              child: Text(
+                                                'No recommendations found',
                                               ),
-                                              child: CachedNetworkImage(
-                                                imageUrl:
-                                                    'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                                                placeholder: (context, url) =>
-                                                    const Center(
-                                                  child:
-                                                      CircularProgressIndicator(),
-                                                ),
-                                                errorWidget:
-                                                    (context, url, error) =>
-                                                        const Icon(Icons.error),
-                                              ),
-                                            ),
-                                          ),
-                                        );
-                                      },
-                                      itemCount: recommendations.length,
-                                    ),
-                                  );
+                                            )
+                                          : ListView.builder(
+                                              scrollDirection: Axis.horizontal,
+                                              itemBuilder: (context, index) {
+                                                final movie =
+                                                    recommendations[index];
+                                                return Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(4.0),
+                                                  child: InkWell(
+                                                    onTap: () {
+                                                      Navigator
+                                                          .pushReplacementNamed(
+                                                        context,
+                                                        MovieDetailPage
+                                                            .routeName,
+                                                        arguments: movie.id,
+                                                      );
+                                                    },
+                                                    child: ClipRRect(
+                                                      borderRadius:
+                                                          const BorderRadius
+                                                              .all(
+                                                        Radius.circular(8),
+                                                      ),
+                                                      child: CachedNetworkImage(
+                                                        imageUrl:
+                                                            'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                                                        placeholder:
+                                                            (context, url) =>
+                                                                const Center(
+                                                          child:
+                                                              CircularProgressIndicator(),
+                                                        ),
+                                                        errorWidget: (context,
+                                                                url, error) =>
+                                                            const Icon(
+                                                                Icons.error),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                );
+                                              },
+                                              itemCount: recommendations.length,
+                                            ));
                                 } else {
                                   return Container();
                                 }
